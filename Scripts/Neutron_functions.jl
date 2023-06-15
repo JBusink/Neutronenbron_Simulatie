63fe11fa85b7a65711528a508906bc9d82311867
JBusink <94122040+JBusink@users.noreply.github.com>
2023-06-15 21:48:28 +0200

    Initialize git repository

---
+++ Scripts/Neutron_functions.jl
@@ -0,0 +1,69 @@
+module NF
+
+using CSV, DataFrames, Interpolations
+
+Nav = 6.023 * 1e23
+MH20 = 18.01524
+n_protons = (1e6 * 0.99820 / MH20) * 2 * Nav#(= protons/m^3)
+n_oxygen = (1e6 * 0.99820 / MH20) * Nav #(= protons/m^3)
+
+
+function logrange(start, stop, step, base=10)
+    return base .^ range(start, stop, step)
+end
+
+function MFP(n, sigma)
+    """Calculates the MFP, preferably in cm.
+
+    Args:
+        n (float): the number density in atoms/cm^3
+        sigma (float): the cross section in barn (10^-24 cm^2)/atom
+
+    Returns:
+        lambda (float): The mean free path in cm, 
+        this is the average distance travelled before a collision (elastic/inelastic)
+    """
+    return (1 / (n * sigma))
+end
+
+function cross_section_H1_SI(E, E0=4.5e4, ET=0.04)
+    """   Calculates the cross section of elastic neutron scattering with hydrogen atoms.
+    Data from stralingshygiene book.
+
+    Args:
+        E (float): Energy of neutron in eV.
+        E0 (float, optional): upperbound of the plateau. Defaults to 4.5e4.
+        ET (float, optional): lower bound of the plateau, thermal energyscale, default value is kbT. Defaults to 0.04.
+
+    Returns:
+        float: cross section value. The value is in units of m^2?
+    """
+    sigma_plateau = 20.0 #eV
+    cross_section = sigma_plateau * (E0^2 / (E0^2 + E^2) + ET^2 / E^2)^0.25
+    return cross_section / 1e28
+end
+
+function read_cross_section_data(file)
+    df = CSV.read(file, DataFrame, header=["Frame", "Energy_ev", "cross_section_b"], skipto=10, delim="    ",)
+    df.cross_section_b = [parse(Float64, strip(df.cross_section_b[i], ['\\', '}'])) for i in 1:1:length(df.cross_section_b)]
+    return df
+end
+
+function cross_section_interpolated(files, Energy)
+    df = read_cross_section_data(files)
+    x, y = df.Energy_ev, df.cross_section_b
+    interpolated = linear_interpolation(x, y)
+    return interpolated(Energy)
+end
+
+
+function random_distance_trav(Energy)
+    λ0 = MFP(n_protons, cross_section_H1_SI(Energy))
+    d = Exponential(λ0)
+    l = rand(d)
+    return l
+end
+
+
+
+end
---
+++ Scripts/Simulation.jl
@@ -0,0 +1,130 @@
+module SM
+
+using Distributions, LinearAlgebra
+
+function unit_vector(theta, phi)
+    """
+    Return a unit vector with polar angles theta and phi.
+    """
+    a = cos(theta)
+    b = sin(theta)
+    c = cos(phi)
+    d = sin(phi)
+    return Array([b * c, b * d, a])
+end
+
+function collision_lab_x(Energy)
+    pmagnitude = sqrt.(2 * Energy)
+
+    θ = (rand()) * pi
+    ϕ = rand() * 2 * pi
+    p_lab = pmagnitude * Array([1, 0, 0]) #collision in arbitrary x dimension.
+    p_cm = p_lab / 2.0
+    p_cm_new = norm(p_cm) * unit_vector(θ, ϕ)
+    p_lab_new = p_cm_new .+ p_cm
+    Energy_new = norm(p_lab_new)^2 ./ 2.0
+    return (Energy_new, θ, ϕ)
+end
+
+function collision_lab_z(Energy)
+    pmagnitude = sqrt.(2 * Energy)
+
+    θ = (rand()) * pi
+    ϕ = rand() * 2 * pi
+    p_lab = pmagnitude * Array([0, 0, 1]) #collision in arbitrary x dimension.
+    p_cm = p_lab / 2.0
+    p_cm_new = norm(p_cm) * unit_vector(θ, ϕ)
+    p_lab_new = p_cm_new .+ p_cm
+    Energy_new = norm(p_lab_new)^2 ./ 2.0
+    return (Energy_new, θ, ϕ)
+end
+
+function single_collision_random(P_initial, E_initial)
+    pmagnitude = norm(P_initial)
+    E = pmagnitude .^ 2 ./ 2.0
+
+    θ = (rand()) * pi
+    ϕ = rand() * 2 * pi
+    p_lab = P_initial
+    p_cm = p_lab / 2.0
+
+    p_cm_new = norm(p_cm) * unit_vector(θ, ϕ)
+    p_lab_new = p_cm_new .+ p_cm
+    Energy_new = norm(p_lab_new)^2 ./ 2.0
+    return (p_lab_new, Energy_new)
+end
+
+function Maxwell_Boltzmann(T)
+    """A Maxwell-Boltzmann distribution of energies is equivalent 
+    to a Gamma distributions with k-shape factor 3/2 and scaling of KbT"""
+    Kb = 8.617343 * 1e-5 # ev/K
+    T = 293
+    d = Distributions.Gamma(3 / 2, Kb .* T)
+    E0 = rand(d, 1)
+    return E0
+end
+
+function multiple_collision(E0, n)
+    i = 0
+    ET = 4e-2
+    T = 293
+    Elist = Vector{Float64}()
+    collision_number = Vector{Float64}()
+    push!(Elist, E0)
+    push!(collision_number, 0)
+    Thermalization_number = Vector{Float64}()
+
+    while i < n
+        E0, θ, ϕ = collision_lab_z(E0)
+        if E0 < ET
+            E0 = Maxwell_Boltzmann(T)[1]
+            # println(i)
+            push!(Thermalization_number, i)
+        end
+
+        push!(Elist, E0)
+        push!(collision_number, 0)
+        i += 1
+    end
+    return Elist, collision_number, Thermalization_number
+end
+
+function multiple_collision_random(P_initial, E_initial, n, thermal,ET)
+    Energylist, Px, Py, Pz = Vector{Float64}(), Vector{Float64}(), Vector{Float64}(), Vector{Float64}()
+    Thermalization_number = Vector{Float64}()
+    collision_number = Vector{Float64}()
+
+    append!(collision_number, 0)
+    append!(Energylist, E_initial)
+    append!(Px, P_initial[1])
+    append!(Py, P_initial[2])
+    append!(Pz, P_initial[3])
+
+    i = 0
+    # ET = 4e-2
+    T = 293
+    pmagnitude = norm(P_initial)
+    E = pmagnitude .^ 2 ./ 2.0
+
+    while i < n
+        P_initial, E = single_collision_random(P_initial, E)
+
+        if thermal == 1
+            if E < ET
+                E = Maxwell_Boltzmann(T)[1]
+                push!(Thermalization_number, i)
+            end
+        end
+
+        append!(Energylist, E)
+        append!(Px, P_initial[1])
+        append!(Py, P_initial[2])
+        append!(Pz, P_initial[3])
+        append!(collision_number, i)
+        i = i .+ 1
+    end
+    return Energylist, Px, Py, Pz, collision_number, Thermalization_number
+end
+
+# function save_single_trajectory()
+end
